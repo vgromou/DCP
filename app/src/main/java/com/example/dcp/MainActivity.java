d1f6c728c72da63069f86dd9a5055ab8485b08f9
vgromou <vgromou@users.noreply.github.com>
2019-11-28 22:02:19 +0300

    ver 0.1

--- app/src/main/java/com/example/dcp/MainActivity.java
+++ app/src/main/java/com/example/dcp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.dcp;
 
+
+import android.view.WindowManager;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        getWindow().getDecorView().setSystemUiVisibility(SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        EditText editText = (EditText) findViewById(R.id.edittext);
+        editText.requestFocus();
+        editText.setShowSoftInputOnFocus(false);
     }
+
 }
